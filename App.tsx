915a6061f5dee7ef2d9f7c50a07f75d26903b63e
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2025-11-15 22:21:31 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -7,28 +7,6 @@ import Location from './components/Location';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
-  const [location, setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
-  const [locationError, setLocationError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error("Geolocation error:", error);
-          setLocationError("Could not get your location. Some features may be limited.");
-        }
-      );
-    } else {
-      setLocationError("Geolocation is not supported by your browser.");
-    }
-  }, []);
-  
   // Effect to manage lucide icons after initial render
   useEffect(() => {
     // @ts-ignore
@@ -45,7 +23,7 @@ const App: React.FC = () => {
         <Hero />
         <Services />
         <Gallery />
-        <Location userLocation={location} locationError={locationError} />
+        <Location />
       </main>
       <Footer />
     </div>
